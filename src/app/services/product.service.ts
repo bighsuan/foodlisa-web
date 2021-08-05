fb7f0ec2fd461a54c0e2eb8bb07e74d6046587aa
bighsuan <aa860221g@gmail.com>
2021-08-05 21:24:49 +0800

    1 file modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { IProduct } from '../models';
+import { StorageService } from './storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
@@ -11,10 +12,11 @@ export class ProductService {
   // private baseUrl = "http://localhost/api/v1";
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json',
+    Authorization: 'bearer '+this.storageService.getToken() })
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private storageService: StorageService) {}
 
   /** GET products from the server */
   getProducts(): Observable<IProduct[]> {
