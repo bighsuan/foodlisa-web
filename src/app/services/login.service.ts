54acacc5de9b1327259824907435a3aa1755bef2
bighsuan <aa860221g@gmail.com>
2021-08-05 10:03:18 +0800

    2 files modified, 1 deleted

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,9 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { UserService } from './user.service';
-import * as JsEncryptModule from 'jsencrypt';
-
-
+import * as forge from 'node-forge';
 
 @Injectable({
   providedIn: 'root'
@@ -14,52 +12,45 @@ export class LoginService {
   private baseUrl = 'https://foodlisa.sytes.net/api/v1';
   // private baseUrl = "http://localhost/api/v1";
 
-  publicKey: string|null = null;
-  encrypt = new JsEncryptModule.JSEncrypt();
+  publicKey: string | null = null;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor(private http: HttpClient,
-    private userService: UserService) {}
+  constructor(private http: HttpClient, private userService: UserService) {}
 
+  getPublicKey(): Observable<any> {
+    const url = `/publickey`;
 
-  getPublicKey():  Observable<any>{
-      
-      const url = `/publickey`; 
-
-      return this.http.get<any>(this.baseUrl+url).pipe(catchError(this.handleError));
+    return this.http
+      .get<any>(this.baseUrl + url)
+      .pipe(catchError(this.handleError));
   }
 
-  getToken(postData:any):Observable<any>{
-    
-    if(this.publicKey != null)
-    {
-      console.log(this.encrypt);
-      this.encrypt.setPublicKey(this.publicKey);
-      let encodePassword = this.encrypt.encrypt(postData.password);
-      console.log(encodePassword);
-      postData.password = encodePassword;
+  getToken(postData: any): Observable<any> {
+    if (this.publicKey != null) {
+      var rsa = forge.pki.publicKeyFromPem(this.publicKey);
+      var encryPassword = window.btoa(rsa.encrypt(postData.password));
+
+      console.log(rsa);
+      console.log(encryPassword);
+      postData.password = encryPassword;
     }
     const url = `${this.baseUrl}/sessions`;
 
-    return this.http.post<any>(
-      url,
-      postData,
-      this.httpOptions
-    ).pipe(catchError(this.handleError));
-
+    return this.http
+      .post<any>(url, postData, this.httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
-  login(postData: any): Observable<any>{
-    this.getPublicKey()
-    .subscribe(publicKey => this.publicKey = publicKey);
-    return this.getToken(postData)
+  login(postData: any): Observable<any> {
+    this.getPublicKey().subscribe(publicKey => (this.publicKey = publicKey));
+    return this.getToken(postData);
   }
 
   logout() {
-    this.userService.delUser();  
+    this.userService.delUser();
   }
 
   // logout(): Observable<any> {
@@ -78,6 +69,4 @@ export class LoginService {
       return of(result as T);
     };
   }
-
-  
 }
